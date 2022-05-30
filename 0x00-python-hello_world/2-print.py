d87908f4792a342a0faa61df82d72c43d5e3cc12
demex1 <komoladefemilade@gmail.com>
2022-05-30 09:44:54 -0700

    first python

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def main():
- print("Programming is like building a multilingual puzzle\n",end=' ')
- return(0)
+    print("Programming is like building a multilingual puzzle\n",end=' ')
+    return(0)
 if __name__ == "__main__": main()
