71ecf57bfd4859950d7f8d1c81b3d5dc5f708576
demex1 <komoladefemilade@gmail.com>
2022-05-30 10:32:07 -0700

    third

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-def main():
-print("Programming is like building a multilingual puzzle\n",end=' ')
-return(0)
-if __name__ == "__main__": main()
+print("Programming is like building a multilingual puzzle\n", end=' ')
