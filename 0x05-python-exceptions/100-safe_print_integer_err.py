4d26dac7793acd7aa76056247a9d9dea42bcb779
demex1 <komolafedemilade2@gmail.com>
2022-06-21 12:22:52 -0700

    rep15

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError  as Er:
-        print("Exception: {}".format(Er),file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
